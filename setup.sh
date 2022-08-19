88b9be850941d04e4262355cc509561d6d4a3d35
Bas Bouwhuis <bas.bouwhuis@medapp.nl>
2022-08-19 18:32:24 +0200

    Prettier script

--- setup.sh
+++ setup.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 . "$PWD/util/utils.sh"
 
-printf "\nChecking for sudo access...\n\n"
-
+print_in_yellow "\nChecking for sudo access...\n"
 ask_for_sudo
+print_in_green "Done!\n"
 
-printf "\nInstalling brew...\n\n"
 
-sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+print_in_yellow "\n(Re)installing brew...\n"
+echo | sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash > /dev/null
 eval "$(/opt/homebrew/bin/brew shellenv)"
+print_in_green "Done!\n"
 
 if [ ! -d ~/.config ]; then
-	printf "\nCreating config folder.."
+	print_in_yellow "\nCreating config folder..\n"
 	sudo mkdir ~/.config
+    print_in_green "Done!\n"
 else
-	printf "\nRecreating config folder.."
+	printf "\nRecreating config folder..\n"
     sudo rm -rf ~/.config
 	sudo mkdir ~/.config
+    print_in_green "Done!\n"
 fi
 
 # Set fish config before doing anything else
 # This is needed for brew to work
 sudo ln -s "$PWD/.config/fish" ~/.config
 
-printf "\nInstalling packages...\n\n"
+print_in_yellow "\nInstalling packages...\n\n"
 
-brew install kitty
-brew install --HEAD neovim
-brew install fish
-brew install spotify-tui
+install_package "kitty"
+install_package "--HEAD neovim"
+install_package "fish"
+install_package "spotify-tui"
 
 if [ "$SHELL" != "/opt/homebrew/bin/fish" ]; then
-    printf "\nSetting fish as your default shell\n\n"
+    print_in_yellow "\nSetting fish as your default shell\n\n"
 
     sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 	chsh -s /opt/homebrew/bin/fish
 fi
 
-printf "\nCreating symlinks...\n\n"
+print_in_yellow "\nCreating symlinks...\n"
 
 sudo ln -s "$PWD/.config/nvim" ~/.config
-
 if [ -d ~/.config/kitty ]; then 
     rm -rf ~/.config/kitty
 fi
-
 sudo ln -s "$PWD/.config/kitty" ~/.config
+
+print_in_green "Done!\n"
--- util/utils.sh
+++ util/utils.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ask_for_sudo() {
 
     # Ask for the administrator password upfront.
@@ -15,4 +17,25 @@ ask_for_sudo() {
         kill -0 "$$" || exit
     done &> /dev/null &
 
+}
+
+install_package() {
+    print_in_yellow "Installing $1...\n"
+    brew install $1 -q
+    print_in_green "Done!\n"
+}
+
+print_in_color() {
+    printf "%b" \
+        "$(tput setaf "$2" 2> /dev/null)" \
+        "$1" \
+        "$(tput sgr0 2> /dev/null)"
+}
+
+print_in_green() {
+    print_in_color "$1" 2
+}
+
+print_in_yellow() {
+    print_in_color "$1" 3
 }
