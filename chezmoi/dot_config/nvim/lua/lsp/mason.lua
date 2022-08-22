b5724eafda2b6a5c3ec0bf19b1d560827728d184
Bas Bouwhuis <bas.bouwhuis@medapp.nl>
2022-08-22 21:20:07 +0200

    Merge?

--- chezmoi/dot_config/nvim/lua/lsp/mason.lua
+++ chezmoi/dot_config/nvim/lua/lsp/mason.lua
@@ -5,51 +5,50 @@ end
 
 local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status_ok_1 then
-    return
+	return
 end
 
 local servers = {
-    "jdtls",
-    "jsonls",
-    "sumneko_lua",
-    "lemminx",
-    "cssls",
-    "cssmodules_ls",
-    "yamlls",
-    "eslint",
-    "tsserver",
-    -- "angular-language-server"
+	"jdtls",
+	"jsonls",
+	"sumneko_lua",
+	"lemminx",
+	"cssls",
+	"cssmodules_ls",
+	"yamlls",
+	"eslint",
+	"tsserver",
 }
 
 local settings = {
-    ui = {
-        border = "rounded",
-        icons = {
-            package_installed = "◍",
-            package_pending = "◍",
-            package_uninstalled = "◍",
-        },
-    },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
+	ui = {
+		border = "rounded",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
 mason.setup(settings)
-mason_lspconfig.setup {
-    ensure_installed = servers,
-    automatic_installation = true,
-}
+mason_lspconfig.setup({
+	ensure_installed = servers,
+	automatic_installation = true,
+})
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-    return
+	return
 end
 
 for _, server in pairs(servers) do
-    local opts = {
-        on_attach = require("lsp.handlers").on_attach,
-        capabilities = require("lsp.handlers").capabilities,
-    }
+	local opts = {
+		on_attach = require("lsp.handlers").on_attach,
+		capabilities = require("lsp.handlers").capabilities,
+	}
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
