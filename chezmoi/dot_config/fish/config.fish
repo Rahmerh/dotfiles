e5661985cefb6ce4eea17085452bea5f466cddba
Bas Bouwhuis <bas@Bass-MacBook-Pro.local>
2022-09-14 18:58:50 +0200

    Switch back to firefox, add some new tools

--- chezmoi/dot_config/fish/config.fish
+++ chezmoi/dot_config/fish/config.fish
@@ -1,4 +1,6 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Aliases
 alias ls='lsd'
 alias la='ls -la'
 alias cat="bat"
@@ -12,3 +14,9 @@ bind \[101\;9u ranger
 
 # Disable greeting message
 set fish_greeting
+
+# Alias & source thefuck
+thefuck --alias | source
+
+# Source autojump
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
