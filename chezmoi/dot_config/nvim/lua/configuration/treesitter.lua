6dae7ea061c79b2c3dbb396fdd9a8a05d68346a8
Bas Bouwhuis <bas.bouwhuis@medapp.nl>
2023-03-07 08:58:19 +0100

    Update treesitter config a bit

--- chezmoi/dot_config/nvim/lua/configuration/treesitter.lua
+++ chezmoi/dot_config/nvim/lua/configuration/treesitter.lua
@@ -4,22 +4,18 @@ if not status_ok then
 end
 
 configs.setup({
-    ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "phpdoc", "tree-sitter-phpdoc" }, -- List of parsers to ignore installing
+    ensure_installed = { "java", "bash", "dockerfile" },
     highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "css" }, -- list of language that will be disabled
+        enable = true,
+        disable = { "css" },
     },
     autopairs = {
         enable = true,
     },
     rainbow = {
         enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
+        extended_mode = true,
+        max_file_lines = nil,
     },
     context_commentstring = {
         enable = true,
