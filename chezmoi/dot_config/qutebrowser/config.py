5298e51b1bef68170ebe0884f61e7cf7c3f79fda
Rahmerh <basbouwhuis@protonmail.com>
2024-02-11 18:58:53 +0100

    Add dxvk conf, couple other changes

--- chezmoi/dot_config/qutebrowser/config.py
+++ chezmoi/dot_config/qutebrowser/config.py
@@ -19,9 +19,5 @@ config.bind('<Ctrl-Shift-p>', 'hint links spawn --detach mpv --force-window yes 
 
 # Appearance
 config.set("window.hide_decoration", True)
-config.set("colors.webpage.darkmode.algorithm", 'lightness-cielab')
-config.set("colors.webpage.darkmode.enabled", True)
-config.set("colors.webpage.preferred_color_scheme", 'dark')
-config.set("colors.webpage.darkmode.policy.images", 'never')
 
 config.source('themes/custom.py')
