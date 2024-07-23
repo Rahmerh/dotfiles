b30ff3fc5434c2285c6ca9d27a300eaa95e41f09
Bas Bouwhuis <basbouwhuis@protonmail.com>
2024-07-23 21:13:57 +0200

    First scripts

--- windows/Apply.ps1
+++ windows/Apply.ps1
@@ -1,1 +1,4 @@
-Write-Host("Test")
+$DotfilesFolder = Join-Path -Path $HOME -ChildPath ".dotfiles";
+
+# Run scripts
+Invoke-Expression (Join-Path -Path $DotfilesFolder -ChildPath "windows" | Join-Path -ChildPath "windows-config" | Join-Path -ChildPath "Windows-Setup.ps1");
