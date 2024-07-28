bcae766b28c441246a80189c62f9ef0a004ac778
Bas Bouwhuis <basbouwhuis@protonmail.com>
2024-07-28 11:37:44 +0200

    Uninstall regular terminal, we be using preview

--- windows/applications/Install-Packages.ps1
+++ windows/applications/Install-Packages.ps1
@@ -1,3 +1,6 @@
 Write-Host "Installing all applications." -ForegroundColor "Cyan";
 
 winget import "$PSScriptRoot\Winget-Export.json"
+
+# Don't need this, we're using the preview
+winget uninstall Microsoft.WindowsTerminal
