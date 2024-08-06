6c0fd65f6ccd33240c4bd6e40203bd56f712755e
Bas Bouwhuis <basbouwhuis@protonmail.com>
2024-08-07 01:18:25 +0200

    Removed lf

--- windows/lf/Lf.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Starting lf installation/configuration" -ForegroundColor "Cyan";
-
-$DotfilesLfFolder = Join-Path -Path $HOME -ChildPath ".dotfiles" | Join-Path -ChildPath "windows" | Join-Path -ChildPath "lf" | Join-Path -ChildPath "configuration";
-
-$LfFolder = Join-Path -Path $env:LOCALAPPDATA -ChildPath lf
-
-Copy-Item -Recurse -Force $DotfilesLfFolder $LfFolder
-
-Write-Host "Finished lf configuration." -ForegroundColor "Green";
