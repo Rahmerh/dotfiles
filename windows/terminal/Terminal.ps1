578890fc895f6ca15fead2baaa4f5567683448a3
Bas Bouwhuis <bas.bouwhuis@smartfloor.com>
2024-10-16 10:03:15 +0200

    New terminal settings

--- windows/terminal/Terminal.ps1
+++ windows/terminal/Terminal.ps1
@@ -1,7 +1,7 @@
 Write-Host "Starting terminal configuration" -ForegroundColor "Cyan";
 
 $DotfilesTerminalFolder = Join-Path -Path $HOME -ChildPath ".dotfiles" | Join-Path -ChildPath "windows" | Join-Path -ChildPath "terminal";
-$TerminalLocalStateFolder = "$HOME\scoop\apps\windows-terminal-preview\1.21.1772.0\settings"
+$TerminalLocalStateFolder = "$HOME\scoop\apps\windows-terminal-preview\current\settings"
 
 Copy-Item -Path "$DotfilesTerminalFolder\\settings.json" -Destination $TerminalLocalStateFolder -Force
 
