2a904049c9be3414e7022566682998c283b8c355
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-05-26 08:56:51 +0200

    Add helper function to more easily add existing config to dotfiles folder

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -25,3 +25,46 @@ set -gx PATH "$HOME/.cargo/bin" $PATH;
 
 # Source zoxide
 zoxide init fish | source
+
+function dot-add
+    if test (count $argv) -lt 1
+        echo "Usage: dot-add <path> [--dry-run]"
+        return 1
+    end
+
+    set dry_run 0
+    for arg in $argv
+        if test $arg = "--dry-run"
+            set dry_run 1
+        end
+    end
+
+    for arg in $argv
+        if not string match -q -- "--*" $arg
+            set full_path (realpath $arg)
+            break
+        end
+    end
+
+    if not test -e $full_path
+        echo "Error: '$full_path' does not exist."
+        return 1
+    end
+
+    set rel_path (string replace -- "$HOME/" "" $full_path)
+    set target_path "$HOME/dotfiles/home/$rel_path"
+
+    echo ""
+    echo "Source: $full_path"
+    echo "Target: $target_path"
+    echo ""
+
+    if test $dry_run -eq 1
+        echo "[DRY RUN] Would move: $full_path → $target_path"
+    else
+        mkdir -p (dirname $target_path)
+        mv $full_path $target_path
+        stow -d ~/dotfiles -t ~ home
+    end
+end
+
