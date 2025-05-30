34d7183197a821761d20cc3d572a7a0f7864b1b8
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-05-30 10:06:53 +0200

    Uncomment stuff (oops) and remove default taskbar

--- install-scripts/03-set-themes.fish
+++ install-scripts/03-set-themes.fish
@@ -1,31 +1,31 @@
 #!/usr/bin/env fish
 source install-scripts/library/print-utils.fish
 
-# print_info "Setting kitty theme"
-#
-# if ! test -d ~/dotfiles/home/.config/kitty/themes/kitty-themes
-#     git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/dotfiles/home/.config/kitty/themes/kitty-themes &> /dev/null
-# else
-#     cd ~/dotfiles/home/.config/kitty/themes/kitty-themes
-#     git pull &> /dev/null
-# end
-#
-# cp ~/dotfiles/home/.config/kitty/themes/kitty-themes/themes/Chalk.conf ~/dotfiles/home/.config/kitty/themes/theme.conf
-#
-# print_success "Done"
-#
-# print_info "Setting sddm theme"
-#
-# if ! test -d /tmp/sddm-theme 
-#     git clone git@github.com:stepanzubkov/where-is-my-sddm-theme.git /tmp/sddm-theme &> /dev/null
-# else
-#     cd /tmp/sddm-theme 
-#     git pull &> /dev/null
-# end
-#
-# sudo /tmp/sddm-theme/install.sh current &> /dev/null
-#
-# print_success "Done"
+print_info "Setting kitty theme"
+
+if ! test -d ~/dotfiles/home/.config/kitty/themes/kitty-themes
+    git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/dotfiles/home/.config/kitty/themes/kitty-themes &> /dev/null
+else
+    cd ~/dotfiles/home/.config/kitty/themes/kitty-themes
+    git pull &> /dev/null
+end
+
+cp ~/dotfiles/home/.config/kitty/themes/kitty-themes/themes/Chalk.conf ~/dotfiles/home/.config/kitty/themes/theme.conf
+
+print_success "Done"
+
+print_info "Setting sddm theme"
+
+if ! test -d /tmp/sddm-theme 
+    git clone git@github.com:stepanzubkov/where-is-my-sddm-theme.git /tmp/sddm-theme &> /dev/null
+else
+    cd /tmp/sddm-theme 
+    git pull &> /dev/null
+end
+
+sudo /tmp/sddm-theme/install.sh current &> /dev/null
+
+print_success "Done"
 
 print_info "Setting firefox theme"
 
@@ -77,5 +77,16 @@ if test "$current" != "$expected"
 
     echo $expected > $statefile
 end
+
+print_success "Done"
+
+print_info "Removing default task bar"
+
+if ! test -e ~/dotfiles/config-backups/plasma-org.kde.plasma.desktop-appletsrc
+    cp ~/.config/plasma-org.kde.plasma.desktop-appletsrc ~/dotfiles/config-backups/
+end
+
+sed -i '/\[Containments\]\[[0-9]*\]/,/^\[/ {/plugin=org.kde.panel/d}' ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+sed -i '/org.kde.plasma.panel/d' ~/.config/plasmashellrc
 
 print_success "Done"
