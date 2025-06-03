6391688148545877a7a116ebcff623c452fdc086
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-03 10:22:08 +0200

    Add qute config

---
+++ home/.config/qutebrowser/config.py
@@ -0,0 +1,5 @@
+config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.darkmode.policy.images", "smart")
+config.set("colors.webpage.darkmode.policy.page", "smart")
+
+config.source('themes/qute-city-lights/city-lights-theme.py')
