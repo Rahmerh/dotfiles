c77a7addf6afc0404d59e27576340703baa6d374
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-03 12:12:17 +0200

    Finished setup script for pass on potential new machines

--- install-scripts/07-setup-password-store.fish
+++ install-scripts/07-setup-password-store.fish
@@ -1,42 +1,44 @@
 #!/usr/bin/env fish
 source install-scripts/library/print-utils.fish
+source install-scripts/library/prompt-utils.fish
 
 mkdir -p ~/.gnupg
 if not test -f ~/.gnupg/gpg.conf
-    print_info "Creating ~/.gnupg/gpg.conf with strong defaults"
-
     echo "use-agent
 personal-digest-preferences SHA512
 cert-digest-algo SHA512
 default-preference-list SHA512 SHA384 SHA256 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed" > ~/.gnupg/gpg.conf
     chmod 600 ~/.gnupg/gpg.conf
-
-    print_success "Done"
 end
 
-print_info "Restarting gpg-agent..."
-gpgconf --kill gpg-agent
-gpgconf --launch gpg-agent
+if ! test -d ~/.password-store/.git
+    print_info "Password store not initialized, starting setup." 
+    print_info "Please input your password store git url:"
+    set password_store_git_url (gum input --placeholder "Insert git url")
+    git clone $password_store_git_url ~/.password-store &> /dev/null
 
-set existing_keys (gpg --list-keys --with-colons 2> /dev/null | grep '^fpr' | cut -d: -f10)
-if test (count $existing_keys) -eq 0
-    print_info "No GPG keys found. Starting key generation..."
-    gpg --full-generate-key
+    echo -e "#!/bin/sh\ngit push origin master > /dev/null 2>&1" > ~/.password-store/.git/hooks/post-commit
+    chmod +x ~/.password-store/.git/hooks/post-commit
+
+    if read_confirm "Do you want to import your gpg key? (Select no if it's already imported)"
+        set keyfile (gum file $HOME)
+        gpg --import $keyfile
+    end
+
     set existing_keys (gpg --list-keys --with-colons 2> /dev/null | grep '^fpr' | cut -d: -f10)
-end
+    set options
+    for fpr in $existing_keys
+        set email (gpg --list-keys --with-colons $fpr | awk -F: -v fpr=$fpr 'BEGIN {found=0} /^fpr:/ {found=($10==fpr)} /^uid:/ && found { print $10; exit }' | grep -oE '<.*>' | tr -d '<>')
+        set -a options "$fpr ($email)"
+    end
 
-print_info "Existing GPG keys:"
-for fpr in $existing_keys
-    set email (gpg --list-keys --with-colons $fpr | awk -F: -v fpr=$fpr 'BEGIN {found=0} /^fpr:/ {found=($10==fpr)} /^uid:/ && found { print $10; exit }' | grep -oE '<.*>' | tr -d '<>')
-    print_text " - $fpr ($email)"
-end
+    print_info "Select the gpg key to use with this store."
+    set selection (gum choose $options)
 
-read -l -p "Enter the GPG key fingerprint to use: " GPG_KEY_FINGERPRINT
-
-set key_exists (gpg --list-keys $GPG_KEY_FINGERPRINT > /dev/null 2>&1; echo $status)
-if test "$key_exists" -ne 0
-    echo "[x] GPG key not found. Aborting."
-    exit 1
+    set GPG_KEY_FINGERPRINT (string match -r '^[A-F0-9]+' -- "$selection")
+    pass init $GPG_KEY_FINGERPRINT
+else
+    print_info "Password store already setup, skipping."
 end
 
 print_success "Done"
