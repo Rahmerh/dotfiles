9d2d43eac63c4facf7bfdaab859169fd78c80c77
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-03 12:55:14 +0200

    Set user agent setting

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -9,3 +9,7 @@ config.set("colors.webpage.darkmode.policy.images", "smart")
 config.set("colors.webpage.darkmode.policy.page", "smart")
 
 config.source('themes/qute-city-lights/city-lights-theme.py')
+
+# Misc settings
+c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+
