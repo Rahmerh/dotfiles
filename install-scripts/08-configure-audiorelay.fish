aae2979d92cc3c07b5e17a0be06563fd76d7d719
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-04 00:29:24 +0200

    Configure some values so it works out of the box... Kinda

--- install-scripts/08-configure-audiorelay.fish
+++ install-scripts/08-configure-audiorelay.fish
@@ -30,4 +30,11 @@ if test $sink_exists -eq 0
     pactl load-module module-null-sink sink_name=audiorelay-out sink_properties=device.description=AudioRelay-Virtual-Speaker
 end
 
+set prefs_path ~/.java/.userPrefs/com/azefsw/audioconnect/prefs.xml
+
+print_info "Apply default settings"
+
+sed -i 's|<entry key="device_capture_id" value="[^"]*"|<entry key="device_capture_id" value="AudioRelay-Virtual-Speaker"|' $prefs_path
+sed -i 's|<entry key="dark_mode" value="[^"]*"|<entry key="dark_mode" value="true"|' $prefs_path
+
 print_success "Done"
