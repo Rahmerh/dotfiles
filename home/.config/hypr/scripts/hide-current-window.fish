1fe4a34a407020515c9f77aa13a10cd0bd819399
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-05 11:06:39 +0200

    Fixed smalle inconsistencies

--- home/.config/hypr/scripts/hide-current-window.fish
+++ home/.config/hypr/scripts/hide-current-window.fish
@@ -4,4 +4,4 @@ set target_workspace "special:hidden"
 set active_address (hyprctl activewindow -j | jq -r .address)
 
 hyprctl dispatch focuswindow address:$active_address
-hyprctl dispatch movetoworkspacesilent special:hidden
+hyprctl dispatch movetoworkspacesilent $target_workspace
--- home/.config/hypr/scripts/restore-hidden-windows.fish
+++ home/.config/hypr/scripts/restore-hidden-windows.fish
@@ -1,7 +1,5 @@
 #!/usr/bin/env fish
 
-set current_workspace (hyprctl activeworkspace -j | jq -r .name)
-
 set selection (
   hyprctl clients -j | jq -r '.[] | select(.workspace.name == "special:hidden") | "\(.address) \(.title)"' \
   | rofi -dmenu -p "Restore window"
@@ -13,5 +11,5 @@ end
 
 set address (echo $selection | awk '{print $1}')
 
-hyprctl dispatch movetoworkspacesilent $current_workspace,address:$address
+hyprctl dispatch movetoworkspacesilent (hyprctl activeworkspace -j | jq -r .name),address:$address
 hyprctl dispatch focuswindow address:$address
