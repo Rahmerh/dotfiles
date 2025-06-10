38e8282a98f797d23b16062cfe1193c91948456b
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-10 20:57:34 +0200

    No need to run it twice...

--- home/.config/nvim/lua/lsp/servers/fish.lua
+++ home/.config/nvim/lua/lsp/servers/fish.lua
@@ -5,7 +5,7 @@ local configs = require("lspconfig.configs")
 if not configs.fish then
     configs.fish = {
         default_config = {
-            cmd = { "fish-lsp" },
+            cmd = { "fish-lsp", "start" },
             filetypes = { "fish" },
             root_dir = lspconfig.util.find_git_ancestor,
             single_file_support = true,
@@ -16,15 +16,4 @@ end
 lspconfig.fish.setup({
     on_attach = config.on_attach,
     capabilities = config.capabilities,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'fish',
-  callback = function()
-    vim.lsp.start({
-      name = 'fish-lsp',
-      cmd = { 'fish-lsp', 'start' },
-      cmd_env = { fish_lsp_show_client_popups = false },
-    })
-  end,
 })
