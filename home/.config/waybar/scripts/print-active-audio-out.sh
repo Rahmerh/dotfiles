2d1ce94a1ccae444ac190267f2d44cbd1353e845
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-15 00:23:18 +0200

    Print on change, instead every 5s

--- home/.config/waybar/scripts/print-active-audio-out.sh
+++ home/.config/waybar/scripts/print-active-audio-out.sh
@@ -4,10 +4,19 @@ declare -A known_sinks
 known_sinks["AudioRelay-Virtual-Speaker"]="Audiorelay"
 known_sinks["Built-in Audio Analog Stereo"]="Speakers"
 
-active_sink=$(pactl list sinks | awk '$1 == "State:" && $2 == "RUNNING" { in_running_sink = 1 } in_running_sink && $1 == "Description:" { print substr($0, index($0,$2)); exit } ')
+get_sink_name() {
+    active_sink=$(pactl list sinks | awk '$1 == "State:" && $2 == "RUNNING" { in_running_sink = 1 } in_running_sink && $1 == "Description:" { print substr($0, index($0,$2)); exit }')
 
-if [[ -n "${known_sinks[$active_sink]}" ]]; then
-    echo "${known_sinks[$active_sink]}"
-else
-    echo "$active_sink"
-fi
+    if [[ -n "${known_sinks[$active_sink]}" ]]; then
+        echo "${known_sinks[$active_sink]}"
+    else
+        echo "$active_sink"
+    fi
+}
+
+get_sink_name
+
+pactl subscribe | grep --line-buffered "Event 'change' on sink" | while read -r _; do
+    get_sink_name
+done
+
