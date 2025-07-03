3a7a3dca6871745367796b73cca0fd43bd354906
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-07-03 08:42:40 +0200

    Now also untars

--- home/.config/fish/functions/utility.fish
+++ home/.config/fish/functions/utility.fish
@@ -51,29 +51,63 @@ function why
 end
 
 function smart-tar
-    if test (count $argv) -lt 2
-        echo "Usage: smart-tar [--dereference] <archive-name> <files...>"
+    if test (count $argv) -lt 1
+        echo "Usage:"
+        echo "  smart-tar <archive-name> <files...>      # create archive"
+        echo "  smart-tar <archive> [--to <dir>]         # extract archive"
         return 1
     end
 
     set -l dereference false
+    set -l target_dir .
     set -l args
 
-    for arg in $argv
+    for i in (seq (count $argv))
+        set arg $argv[$i]
         switch $arg
             case --dereference
                 set dereference true
+            case --to
+                set i (math $i + 1)
+                set target_dir $argv[$i]
             case '*'
                 set -a args $arg
         end
     end
 
+    # Detect extract mode
+    set -l first $args[1]
+    if test (count $args) -eq 1 -a -f $first
+        switch $first
+            case "*.tar" "*.tar.gz" "*.tgz" "*.tar.xz" "*.tar.bz2"
+                set archive $first
+
+                switch $archive
+                    case "*.tar.gz" "*.tgz"
+                        set tar_flags -xzvf
+                    case "*.tar.bz2"
+                        set tar_flags -xjvf
+                    case "*.tar.xz"
+                        set tar_flags -xJvf
+                    case "*.tar"
+                        set tar_flags -xvf
+                    case '*'
+                        echo "Unsupported archive format: $archive"
+                        return 1
+                end
+
+                tar $tar_flags $archive -C $target_dir
+                return $status
+        end
+    end
+
+    # --- Compression path ---
     if test (count $args) -lt 2
         echo "Missing archive name or file arguments"
         return 1
     end
 
-    set -l archive $args[1]
+    set archive $args[1]
     set -e args[1]
 
     set -l ext
